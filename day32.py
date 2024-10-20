5f23aaaca24bd52ce1bcd4bb7260f95ba7b7172c
soniyajana2623 <soniyajana2623@gmail.com>
2024-10-20 16:37:04 +0530

    testing commit

--- day32.py
+++ day32.py
@@ -4,3 +4,6 @@ print (s1.union(s2)) #union is merging 2 sets
 print(s1.intersection(s2)) #intersection is common value in set 
 print(s1.symmetric_difference(s2)) #symmetric diff matlab jo value common nahi hai
 print(s1.difference(s2))  #isme s1-s2 hota hai 
+
+
+# Adding comments
